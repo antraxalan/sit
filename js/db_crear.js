7e4a145d672228b44bbdff4077d5ecfd8eb23da2
antraxalan <aruquipa.alan@gmail.com>
2015-09-28 16:18:59 -0400

    'orden'

--- js/db_crear.js
+++ js/db_crear.js
@@ -2,6 +2,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 var g_username;
 var g_password;
 function onDeviceReady() {
+	alert("Device Ready is called by exit");
 	var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
 	db.transaction(populateDB, errorCB);
 	inicio_page();
