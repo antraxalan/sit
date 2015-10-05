493c0cd1001cb280989f7a323d47477176f5b74c
antraxalan <aruquipa.alan@gmail.com>
2015-10-05 15:54:56 -0400

    'cli_det'

--- js/cargar/cargar_articulos.js
+++ js/cargar/cargar_articulos.js
@@ -1,5 +1,7 @@
 var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
-var data_db_articulos;
+var data_db_articulo;
+var data_db_cliente;
+var data_db_detalle;
 var id_transp=localStorage.g_username;
 
 function subir_db_articulo() {
@@ -20,8 +22,8 @@ function subir_db_articulo() {
     data: "codigo=" + codigo + "&password=" + password + "&info=" + info,
     success: function (resp) {
         //----- alert("success:"+resp);
-        db.transaction(populateDB_articulos, errorCB_art);
-        call_insert_db_articulos(resp);
+        db.transaction(populateDB_articulo, errorCB_cargar);
+        call_insert_db_articulo(resp);
         // $.mobile.loading("hide");
     },
     error: function (e) {
@@ -34,25 +36,74 @@ function subir_db_articulo() {
   // var dbShell = window.openDatabase(database_name, database_version, database_displayname, database_size);
   // db.transaction(insertDB, errorCB);
 };
+function subir_db_cliente() {
+  var direccion= $(".direccion").val();
+  var codigo      =localStorage.g_username;
+  var password    =localStorage.g_password;
+  var info='cliente';
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: "http://"+direccion+"/sitrans_server/sitrans.php",
+    data: "codigo=" + codigo + "&password=" + password + "&info=" + info,
+    success: function (resp) {
+        db.transaction(populateDB_cliente, errorCB_cargar);
+        call_insert_db_cliente(resp);
+    },
+    error: function (e) {
+      $.mobile.loading("hide");
+  }
+});
+ 
+};
+function subir_db_detalle() {
+  var direccion= $(".direccion").val();
+  var codigo      =localStorage.g_username;
+  var password    =localStorage.g_password;
+  var info='detalle';
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: "http://"+direccion+"/sitrans_server/sitrans.php",
+    data: "codigo=" + codigo + "&password=" + password + "&info=" + info,
+    success: function (resp) {
+        db.transaction(populateDB_detalle, errorCB_cargar);
+        call_insert_db_detalle(resp);
+    },
+    error: function (e) {
+      $.mobile.loading("hide");
+  }
+});
+};
 
-function populateDB_articulos(tx) {
-    //----- alert("popul");
+function populateDB_articulo(tx) {
+    //----- alert("19");
     tx.executeSql('DROP TABLE IF EXISTS ARTICULO');
     tx.executeSql('CREATE TABLE IF NOT EXISTS ARTICULO (CodMarca,DesMarca,CodArt,DesArt,DesArtReducido,Calibre,TipoArticulo,CantxEmpaque,PrecioCompra,PrecioVtaMin,PrecioVtaMax,CodBotella,DesBotella,PVtaMinBot,CodCaja,DesCaja,PVtaMinCaja,PVtaMaxCaja,Estado)');
 }
+function populateDB_cliente(tx) {
+    //----- alert("13");
+    tx.executeSql('DROP TABLE IF EXISTS CLIENTE');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS CLIENTE (CodCliente,Nombre,RazonSocial,Direccion,Nit,NroTelefono1,NroTelefono2,CodZona,DesZona,CodPersonal,DesPersonal,CodRuta,DesRuta)');
+}
+function populateDB_detalle(tx) {
+    //----- alert("18");
+    tx.executeSql('DROP TABLE IF EXISTS DETALLE');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS DETALLE (TipoDcto,NroDcto,Apu,Fecha,FechaVto,TipoDctoM,NroDctoM,Precio,Tc,CodConcepto,CodCliente,Debe,Haber,CodArt,Dcajas,Hcajas,Dunidades,Hunidades)');
+}
 
 
-function call_insert_db_articulos(data) {
-    data_db_articulos=data;
-    //----- alert("call_insert_db_articulos");
+
+function call_insert_db_articulo(data) {
+    data_db_articulo=data;
+    //----- alert("call_insert_db_articulo");
     // codigo_usuario=user;
     // password_usuario=pass;
     // var dbShell = window.openDatabase(database_name, database_version, database_displayname, database_size);
-    db.transaction(insertDB_articulos, errorCB1_art);
+    db.transaction(insertDB_articulo, errorCB1_carg2);
 };
-
-function insertDB_articulos(tx) {
-    var d1=data_db_articulos;
+function insertDB_articulo(tx) {
+    var d1=data_db_articulo;
     //----- alert("insertando: "+d1);
 
     //----- alert("-longitud1: "+d1[0].length);
@@ -69,7 +120,7 @@ function insertDB_articulos(tx) {
 
         tx.executeSql('INSERT INTO ARTICULO (CodMarca,DesMarca,CodArt,DesArt,DesArtReducido,Calibre,TipoArticulo,CantxEmpaque,PrecioCompra,PrecioVtaMin,PrecioVtaMax,CodBotella,DesBotella,PVtaMinBot,CodCaja,DesCaja,PVtaMinCaja,PVtaMaxCaja,Estado) VALUES ("'+d1[i][0]+'","'+d1[i][1]+'","'+d1[i][2]+'","'+d1[i][3]+'","'+d1[i][4]+'","'+d1[i][5]+'","'+d1[i][6]+'","'+d1[i][7]+'","'+d1[i][8]+'","'+d1[i][9]+'","'+d1[i][10]+'","'+d1[i][11]+'","'+d1[i][12]+'","'+d1[i][13]+'","'+d1[i][14]+'","'+d1[i][15]+'","'+d1[i][16]+'","'+d1[i][17]+'","'+d1[i][18]+'")');
     };
-    alert("carga finalizada");
+    alert("carga articulo finalizada");
     // localStorage.g_username = codigo_usuario;
     // localStorage.g_password = password_usuario;
     // localStorage.g_existe = 1;
@@ -77,12 +128,37 @@ function insertDB_articulos(tx) {
     // tx.executeSql('INSERT INTO USUARIO (id,codigo,pass) VALUES (1,"11","22")');
 };
 
+function call_insert_db_cliente(data) {
+    data_db_cliente=data;
+    db.transaction(insertDB_cliente, errorCB1_carg2);
+};
 
-function errorCB_art(err) {
-    //----- alert("errorCB_art: "+err.message);
+function insertDB_cliente(tx) {
+    var d1=data_db_cliente;
+    for (var i = 0; i < d1.length; i++) {
+        tx.executeSql('INSERT INTO CLIENTE (CodCliente,Nombre,RazonSocial,Direccion,Nit,NroTelefono1,NroTelefono2,CodZona,DesZona,CodPersonal,DesPersonal,CodRuta,DesRuta) VALUES ("'+d1[i][0]+'","'+d1[i][1]+'","'+d1[i][2]+'","'+d1[i][3]+'","'+d1[i][4]+'","'+d1[i][5]+'","'+d1[i][6]+'","'+d1[i][7]+'","'+d1[i][8]+'","'+d1[i][9]+'","'+d1[i][10]+'","'+d1[i][11]+'","'+d1[i][12]+'")');
+    };
+    alert("carga cliente finalizada");
+};
+function call_insert_db_detalle(data) {
+    data_db_detalle=data;
+    db.transaction(insertDB_detalle, errorCB1_carg2);
+};
+
+function insertDB_detalle(tx) {
+    var d1=data_db_detalle;
+    for (var i = 0; i < d1.length; i++) {
+        tx.executeSql('INSERT INTO DETALLE (TipoDcto,NroDcto,Apu,Fecha,FechaVto,TipoDctoM,NroDctoM,Precio,Tc,CodConcepto,CodCliente,Debe,Haber,CodArt,Dcajas,Hcajas,Dunidades,Hunidades) VALUES ("'+d1[i][0]+'","'+d1[i][1]+'","'+d1[i][2]+'","'+d1[i][3]+'","'+d1[i][4]+'","'+d1[i][5]+'","'+d1[i][6]+'","'+d1[i][7]+'","'+d1[i][8]+'","'+d1[i][9]+'","'+d1[i][10]+'","'+d1[i][11]+'","'+d1[i][12]+'","'+d1[i][12]+'","'+d1[i][13]+'","'+d1[i][14]+'","'+d1[i][15]+'","'+d1[i][16]+'","'+d1[i][17]+'")');
+    };
+    alert("carga detalle finalizada");
+};
+
+
+function errorCB_cargar(err) {
+    //----- alert("errorCB_cargar: "+err.message);
 }
-function errorCB1_art(err) {
-    //----- alert("errorCB1_art: "+err.message);
+function errorCB1_carg2(err) {
+    //----- alert("errorCB1_carg2: "+err.message);
 }
 
 
@@ -105,7 +181,8 @@ function verificar_usuario() {
     success: function (resp) {
         if(resp=='1'){
             subir_db_articulo();
-            // subir_db_articulo();
+            subir_db_cliente();
+            subir_db_detalle();
             $.mobile.loading("hide");
         }else{
             $.mobile.loading("hide");
--- js/info/mostrar.js
+++ js/info/mostrar.js
@@ -1,17 +1,29 @@
 
 
 		var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
-		var currentRow;
+
         // Populate the database
         //
-        function populateDB_info(tx) {
+        function populateDB_info_articulo(tx) {
         	tx.executeSql('CREATE TABLE IF NOT EXISTS ARTICULO (CodMarca,DesMarca,CodArt,DesArt,DesArtReducido,Calibre,TipoArticulo,CantxEmpaque,PrecioCompra,PrecioVtaMin,PrecioVtaMax,CodBotella,DesBotella,PVtaMinBot,CodCaja,DesCaja,PVtaMinCaja,PVtaMaxCaja,Estado)');
+        }
+        function populateDB_info_cliente(tx) {
+        	tx.executeSql('CREATE TABLE IF NOT EXISTS CLIENTE (CodCliente,Nombre,RazonSocial,Direccion,Nit,NroTelefono1,NroTelefono2,CodZona,DesZona,CodPersonal,DesPersonal,CodRuta,DesRuta)');
+        }
+        function populateDB_info_detalle(tx) {
+        	tx.executeSql('CREATE TABLE IF NOT EXISTS DETALLE (TipoDcto,NroDcto,Apu,Fecha,FechaVto,TipoDctoM,NroDctoM,Precio,Tc,CodConcepto,CodCliente,Debe,Haber,CodArt,Dcajas,Hcajas,Dunidades,Hunidades)');
         }
 
         // Query the database
         //
-        function queryDB_info(tx) {
-        	tx.executeSql('SELECT * FROM ARTICULO', [], querySuccess_info, errorCB_info);
+        function queryDB_info_articulo(tx) {
+        	tx.executeSql('SELECT * FROM ARTICULO', [], querySuccess_info_articulo, errorCB_info);
+        }
+        function queryDB_info_cliente(tx) {
+        	tx.executeSql('SELECT * FROM CLIENTE', [], querySuccess_info_cliente, errorCB_info);
+        }
+        function queryDB_info_detalle(tx) {
+        	tx.executeSql('SELECT * FROM DETALLE', [], querySuccess_info_detalle, errorCB_info);
         }
 
         // function searchQueryDB(tx) {
@@ -21,22 +33,22 @@
 
         // Query the success callback
         //
-        function querySuccess_info(tx, results) {
+        function querySuccess_info_articulo(tx, results) {
         	// var tblText='<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
         	var tblContent='<form>';
-			tblContent+='<input id="filterTable-input" data-type="search">';
-			tblContent+='</form>';
-			tblContent+='<table data-role="table" id="movie-table" data-mode="columntoggle" data-filter="true" data-input="#filterTable-input" class="ui-responsive">';
-			tblContent+='<thead>';
-			tblContent+='<tr>';
+        	tblContent+='<input id="filterTable-input" data-type="search">';
+        	tblContent+='</form>';
+        	tblContent+='<table data-role="table" id="movie-table" data-mode="columntoggle" data-filter="true" data-input="#filterTable-input" class="ui-responsive">';
+        	tblContent+='<thead>';
+        	tblContent+='<tr>';
 
-			tblContent+='<th data-priority="persist">CodMarca</th>';
-			tblContent+='<th data-priority="2">DesMarca</th>';
-			tblContent+='<th data-priority="3">CodArt</th>';
-			tblContent+='<th data-priority="1">DesArt</th>';
-			tblContent+='</tr>';
-			tblContent+='</thead>';
-			tblContent+='<tbody>';
+        	tblContent+='<th data-priority="persist">CodMarca</th>';
+        	tblContent+='<th data-priority="2">DesMarca</th>';
+        	tblContent+='<th data-priority="3">CodArt</th>';
+        	tblContent+='<th data-priority="1">DesArt</th>';
+        	tblContent+='</tr>';
+        	tblContent+='</thead>';
+        	tblContent+='<tbody>';
 
 
         	var len = results.rows.length;
@@ -50,13 +62,85 @@
         		tblContent+=results.rows.item(i).DesArt	 	+'</td></tr>';
         	}
         	tblContent+="</tbody></table>";
-        	document.getElementById("tabla_info").innerHTML =tblContent;
-        	// $('#tabla_info').html(tblContent);
+        	// document.getElementById("tabla_info").innerHTML =tblContent;
+        	$('#tabla_info').html(tblContent);
         	 // $('#tabla_info').append(tblContent);
-        	$("#tabla_info").trigger("create");
-        }
+        	 $("#tabla_info").trigger("create");
+        	}
 
-        
+        	function querySuccess_info_cliente(tx, results) {
+        	// var tblText='<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
+        	var tblContent='<form>';
+        	tblContent+='<input id="filterTable-input" data-type="search">';
+        	tblContent+='</form>';
+        	tblContent+='<table data-role="table" id="movie-table" data-mode="columntoggle" data-filter="true" data-input="#filterTable-input" class="ui-responsive">';
+        	tblContent+='<thead>';
+        	tblContent+='<tr>';
+
+        	tblContent+='<th data-priority="persist">CodCliente</th>';
+        	tblContent+='<th data-priority="2">Nombre</th>';
+        	tblContent+='<th data-priority="1">RazonSocial</th>';
+        	tblContent+='<th data-priority="3">Direccion</th>';
+        	tblContent+='</tr>';
+        	tblContent+='</thead>';
+        	tblContent+='<tbody>';
+
+
+        	var len = results.rows.length;
+        	for (var i = 0; i < len; i++) {
+
+        		tblContent +='<tr><td>'; 
+
+        		tblContent+=results.rows.item(i).CodCliente		+'</td><td>'
+        		tblContent+=results.rows.item(i).Nombre			+'</td><td>'
+        		tblContent+=results.rows.item(i).RazonSocial	+'</td><td>'
+        		tblContent+=results.rows.item(i).Direccion	 	+'</td></tr>';
+        	}
+        	tblContent+="</tbody></table>";
+        	// document.getElementById("tabla_info").innerHTML =tblContent;
+        	$('#tabla_info').html(tblContent);
+        	// $('#tabla_info').append(tblContent);
+        	$("#tabla_info").trigger("create");
+        	}
+
+        	function querySuccess_info_detalle(tx, results) {
+        	// var tblText='<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
+        	var tblContent='<form>';
+        	tblContent+='<input id="filterTable-input" data-type="search">';
+        	tblContent+='</form>';
+        	tblContent+='<table data-role="table" id="movie-table" data-mode="columntoggle" data-filter="true" data-input="#filterTable-input" class="ui-responsive">';
+        	tblContent+='<thead>';
+        	tblContent+='<tr>';
+
+        	tblContent+='<th data-priority="persist">TipoDcto</th>';
+        	tblContent+='<th data-priority="2">Apu</th>';
+        	tblContent+='<th data-priority="3">Fecha</th>';
+        	tblContent+='<th data-priority="1">FechaVto</th>';
+        	tblContent+='</tr>';
+        	tblContent+='</thead>';
+        	tblContent+='<tbody>';
+
+
+        	var len = results.rows.length;
+        	for (var i = 0; i < len; i++) {
+
+        		tblContent +='<tr><td>'; 
+
+        		tblContent+=results.rows.item(i).TipoDcto		+'</td><td>'
+        		tblContent+=results.rows.item(i).Apu			+'</td><td>'
+        		tblContent+=results.rows.item(i).Fecha			+'</td><td>'
+        		tblContent+=results.rows.item(i).FechaVto	 	+'</td></tr>';
+        	}
+        	tblContent+="</tbody></table>";
+        	// document.getElementById("tabla_info").innerHTML =tblContent;
+        	$('#tabla_info').html(tblContent);
+        	 // $('#tabla_info').append(tblContent);
+        	 $("#tabla_info").trigger("create");
+        	}
+
+
+
+
 
         // Transaction error callback
         //
@@ -66,18 +150,35 @@
 
         // Transaction success callback
         //
-        function successCB_info() {
-        	// alert("successCB_info");
+        function successCB_info_articulo() {
+        	// alert("successCB_info_articulo");
         	// var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
-        	db.transaction(queryDB_info, errorCB_info);
+        	db.transaction(queryDB_info_articulo, errorCB_info);
+        }
+        function successCB_info_cliente() {
+        	// alert("successCB_info_articulo");
+        	// var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
+        	db.transaction(queryDB_info_cliente, errorCB_info);
+        }
+        function successCB_info_cliente() {
+        	// alert("successCB_info_articulo");
+        	// var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
+        	db.transaction(queryDB_info_detalle, errorCB_info);
         }
 
          // Cordova is ready
         //
-        function cargar_info() {
+        function cargar_info_articulo() {
         	// alert("cargar_info");
-        	
-        	db.transaction(populateDB_info, errorCB_info, successCB_info);
+        	db.transaction(populateDB_info_articulo, errorCB_info, successCB_info_articulo);
+        }
+        function cargar_info_cliente() {
+        	// alert("cargar_info");
+        	db.transaction(populateDB_info_cliente, errorCB_info, successCB_info_cliente);
+        }
+        function cargar_info_detalle() {
+        	// alert("cargar_info");
+        	db.transaction(populateDB_info_detalle, errorCB_info, successCB_info_detalle);
         }
 
-     
+
