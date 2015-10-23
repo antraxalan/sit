8f6c594c926db24b3dfbb8970eb9679d4c620aa5
antraxalan <aruquipa.alan@gmail.com>
2015-10-23 18:03:41 -0400

    script_index.js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,37 @@
     sourceElement.on("popupafterclose", afterClose);
     sourceElement.popup("close");
 };
+
+function carga_varios_complementos(){
+    ///////////////////////////////
+    ///////////////SWIPE///////////
+    ///////////////////////////////
+    // Touchmove events are cancelled on Android KitKat when scrolling is possible on the touched element. 
+    // Scrolling is always vertical in our app. Cancel the event when a touchmove is horizontal, 
+    // so that all following touchmove events will be raised normally. 
+    var startLoc = null; 
+    $( "body" ).on( "touchstart", function( e ) { 
+    if( e.originalEvent.touches.length == 1 ) { // one finger touch 
+    // Remember start location. 
+    var touch = e.originalEvent.touches[ 0 ]; 
+    startLoc = { x : touch.pageX, y : touch.pageY }; 
+    } 
+    }); 
+
+    $( "body" ).on( "touchmove", function( e ) { 
+    // Only check first move after the touchstart. 
+    if( startLoc ) { 
+      var touch = e.originalEvent.touches[ 0 ]; 
+    // Check if the horizontal movement is bigger than the vertical movement. 
+    if( Math.abs( startLoc.x - touch.pageX ) > 
+      Math.abs( startLoc.y - touch.pageY ) ) { 
+    // Prevent default, like scrolling. 
+    e.preventDefault(); 
+    } 
+    startLoc = null; 
+    } 
+    } ); 
+    ///////////////////////////////
+    ///////////////SWIPE///////////
+    ///////////////////////////////
+}
