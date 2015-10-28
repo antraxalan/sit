b378b62e1dcccf4875116e223d7e00598958defb
antraxalan <aruquipa.alan@gmail.com>
2015-10-28 11:28:21 -0400

    exit.js_switch

--- js/exit.js
+++ js/exit.js
@@ -20,14 +20,36 @@ function start_exit(){
 document.addEventListener("backbutton", function(e){
     // alert("back");
     // if ( $('.paginaprincipal').attr('id') == 'home') {
-    if ( $.mobile.activePage.attr('id') == 'home') {
-        // alert("pag princ");
-            //window.location = "#exitDialog";
-            exitAppPopup();
-        }else{
+        var curr=$.mobile.activePage.attr('id'); 
+        // if ( curr == 'home') {
+        // // alert("pag princ");
+        //     //window.location = "#exitDialog";
+        //     exitAppPopup();
+        // }else{
             // alert("history back");
             // history.back();
-           history.back();
+            switch (curr) {
+                case 'home':
+                exitAppPopup();
+                break; 
+                case 'registrar':
+                window.location.href = "index.html#home";
+                break; 
+                case 'venta':
+                otroCliente();
+                // window.location.href = "index.html#registrar";
+                break; 
+                case 'cobranza':
+                window.location.href = "index.html#venta";
+                break; 
+                case 'deuda':
+                window.location.href = "index.html#cobranza";
+                break; 
+
+                default: 
+                history.back();
+            }
+
         };
     });
 };
@@ -45,12 +67,35 @@ function exitAppPopup() {
     // alert("Outside Notification"); 
     //return false;
 };
+function otroCliente() {
+    navigator.notification.vibrate(200);
+    navigator.notification.confirm(
+        "Desea cambiar de cliente?", 
+        function(buttonIndex){
+            ConfirmCliente(buttonIndex);
+        }, 
+        "Confirmación", 
+        "Si,No"
+        ); 
+    // alert("Outside Notification"); 
+    //return false;
+};
 
 function ConfirmExit(stat){
     // alert("Inside ConfirmExit");
     if(stat == "1"){
         // alert("exit app fn");
         navigator.app.exitApp();
+    }else{
+        return;
+    };
+};
+
+function ConfirmCliente(stat){
+    // alert("Inside ConfirmExit");
+    if(stat == "1"){
+        // alert("exit app fn");
+        window.location.href = "index.html#registrar";
     }else{
         return;
     };
