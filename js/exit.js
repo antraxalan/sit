d9783c915bb118606e290fb3e8a594dfd65ee433
antraxalan <aruquipa.alan@gmail.com>
2015-10-28 13:31:43 -0400

    jqm.min

--- js/exit.js
+++ js/exit.js
@@ -1,8 +1,8 @@
-function carga_exit() {
+function carga_navigation() {
     // alert("load exit");
-    document.addEventListener("deviceready", start_exit, false);
+    document.addEventListener("deviceready", start_navigation, false);
 }
-function start_exit(){
+function start_navigation(){
     // alert("Device Ready is called by exit");
 
         // console.log('Device Readyasdasd');
@@ -18,40 +18,34 @@ function start_exit(){
 
 
 document.addEventListener("backbutton", function(e){
-    // alert("back");
-    // if ( $('.paginaprincipal').attr('id') == 'home') {
-        var curr=$.mobile.activePage.attr('id'); 
-        // if ( curr == 'home') {
-        // // alert("pag princ");
-        //     //window.location = "#exitDialog";
-        //     exitAppPopup();
-        // }else{
-            // alert("history back");
-        // history.back();
-        if ($(".ui-page-active .ui-popup-active").length > 0){
-         history.back();
-     }else{
-         switch (curr) {
-            case 'home':
-            exitAppPopup();
-            break; 
-            case 'registrar':
-            window.location.href = "index.html#home";
-            break; 
-            case 'venta':
-            otroCliente();
-            break; 
-            case 'cobranza':
-            window.location.href = "index.html#venta";
-            break; 
-            case 'deuda':
-            window.location.href = "index.html#cobranza";
-            break; 
 
-            default: 
-            history.back();
-        };
+    var curr=$.mobile.activePage.attr('id'); 
+
+    if ($(".ui-page-active .ui-popup-active").length > 0){
+     history.back();
+ }else{
+     switch (curr) 
+     {
+        case 'home':
+        exitAppPopup();
+        break; 
+        case 'registrar':
+        window.location.href = "index.html#home";
+        break; 
+        case 'venta':
+        otroCliente();
+        break; 
+        case 'cobranza':
+        window.location.href = "index.html#venta";
+        break; 
+        case 'deuda':
+        window.location.href = "index.html#cobranza";
+        break; 
+
+        default: 
+        history.back();
     };
+};
 
 });
 };
@@ -70,7 +64,7 @@ function exitAppPopup() {
     //return false;
 };
 function otroCliente() {
-    navigator.notification.vibrate(200);
+    navigator.notification.vibrate(50);
     navigator.notification.confirm(
         "Desea cambiar de cliente?", 
         function(buttonIndex){
@@ -102,5 +96,11 @@ function ConfirmCliente(stat){
         return;
     };
 };
-
-
+function got_to_otro_usuario(){
+    $('#cliente').selectmenu();
+    $("#cliente option:eq(0)").prop("selected",true);
+    $('#cliente') .selectmenu("refresh");
+    $('.tablita_info_cliente').hide();
+    $('.menu_v_c_d').hide();
+    window.location.href = "index.html#registrar";
+};
