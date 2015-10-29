ce2d21fbe9bd0d2de8e55a380e7dbaa2865de131
antraxalan <aruquipa.alan@gmail.com>
2015-10-29 12:01:18 -0400

    plus_price

--- js/cargar/list_ventas.js
+++ js/cargar/list_ventas.js
@@ -11,7 +11,7 @@ var id_cliente;
 
         function queryDB_new_articulo(tx) {
           // alert("new query");
-          tx.executeSql('SELECT CodArt, DesArt, CodMarca from ARTICULO ', [], querySuccess_new_articulo, errorCB_list);
+          tx.executeSql('SELECT CodArt, DesArt, CodMarca, PrecioVtaMax from ARTICULO ', [], querySuccess_new_articulo, errorCB_list);
         }
 
 
@@ -27,8 +27,12 @@ var id_cliente;
             }
             tblContent +='<li><a href="#">';
             tblContent +='<img src="img/marcas/'+co_ma+'.png">';
+            var cod=$(this).attr('codigo-venta');
+            var caj=$(this).attr('cant-cajas');
+            var uni=$(this).attr('cant-unidades');
+            var pri=$(this).attr('last-price');
             tblContent +='<h2>'+results.rows.item(i).CodArt+' - '+results.rows.item(i).DesArt+'</h2></a>';
-            tblContent +='<a href="#add_venta_popup" class="add_venta_popup_class_old" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" data-transition="flow">Historial</a></li>';
+            tblContent +='<a href="#add_venta_popup" class="add_venta_popup_class_old" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" last-price="'+results.rows.item(i).Precio+'" data-transition="flow">Historial</a></li>';
             // tblContent +='<option value="'+results.rows.item(i).CodArt+'">'+results.rows.item(i).CodArt+' - '+results.rows.item(i).DesArt+'</option>'; 
           }
           tblContent +='</ul>';
@@ -60,7 +64,7 @@ var id_cliente;
             tblContent +='<li><a href="#">';
             tblContent +='<img src="img/marcas/'+co_ma+'.png">';
             tblContent +='<h2>'+results.rows.item(i).CodArt+' - '+results.rows.item(i).DesArt+'</h2></a>';
-            tblContent +='<a href="#add_venta_popup" class="add_venta_popup_class_new" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" data-transition="flow">Historial</a></li>';
+            tblContent +='<a href="#add_venta_popup" class="add_venta_popup_class_new" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" last-price="'+results.rows.item(i).PrecioVtaMax+'" data-transition="flow">Historial</a></li>';
             // tblContent +='<option value="'+results.rows.item(i).CodArt+'">'+results.rows.item(i).CodArt+' - '+results.rows.item(i).DesArt+'</option>'; 
           }
           tblContent +='</ul>';
@@ -100,7 +104,7 @@ var id_cliente;
         // Transaction success callback
         //
         function cargar_old_venta_list() {
-          
+
           // alert("successCB_select_articulo");
           // var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
           db.transaction(queryDB_old_articulo, errorCB_list);
