6cf1875022ecd45b268ae5397ce50b2b2bc83c75
antraxalan <aruquipa.alan@gmail.com>
2015-11-09 15:05:54 -0400

    1ertab_completo_cal_empa_inputs

--- js/cargar/list_ventas.js
+++ js/cargar/list_ventas.js
@@ -16,7 +16,7 @@ var cantidad_ids;
 
         function queryDB_new_articulo(tx) {
           // alert("new query");
-          tx.executeSql('select a.CodArt,DesArt,CodMarca,calibre,cantxempaque,PrecioVtaMax,sum(dcajas-hcajas) CajasCamion from articulo a inner join detalle b on a.codart=b.codart and codconcepto=1800 where tipoarticulo="P" group by a.codart,desart,calibre,cantxempaque,preciovtamax,CodMarca order by a.codart', [], querySuccess_new_articulo, errorCB_list);
+          tx.executeSql('select a.CodArt,DesArt,CodMarca,Calibre,CantxEmpaque,PrecioVtaMax,sum(dcajas-hcajas) CajasCamion from articulo a inner join detalle b on a.codart=b.codart and codconcepto=1800 where tipoarticulo="P" group by a.codart,desart,calibre,cantxempaque,preciovtamax,CodMarca order by a.codart', [], querySuccess_new_articulo, errorCB_list);
         }
 
 
@@ -74,7 +74,7 @@ var cantidad_ids;
             lista_contenido +='<p>Ultima venta: <strong>'+results.rows.item(i).Cajas+'</strong> cajas a <strong>'+results.rows.item(i).Importe+'</strong> Bolivianos. (<strong>'+fecha_ultm+'</strong>)</p></a>';
 
 
-            lista_contenido +='<a href="#add_venta_popup" '+disabled+' class="add_venta_popup_class_old" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" last-price="'+precio+'" cajas-camion="'+results.rows.item(i).CajasCamion+'" >Historial</a></li>';
+            lista_contenido +='<a href="#add_venta_popup" '+disabled+' class="add_venta_popup_class_old" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" last-price="'+precio+'" cajas-camion="'+results.rows.item(i).CajasCamion+'" calibre="'+results.rows.item(i).CajasCamion+'" cant-empaque="'+results.rows.item(i).CajasCamion+'"  >Historial</a></li>';
             // lista_contenido +='<option value="'+results.rows.item(i).CodArt+'">'+results.rows.item(i).CodArt+' - '+results.rows.item(i).DesArt+'</option>'; 
           }
           // lista_contenido +='</ul>';
@@ -112,7 +112,7 @@ var cantidad_ids;
               lista_contenido +='<li><a href="#">';
               lista_contenido +='<img src="img/marcas/'+co_ma+'.png">';
               lista_contenido +='<h2>'+results.rows.item(i).CodArt+' - '+results.rows.item(i).DesArt+'</h2></a>';
-              lista_contenido +='<a href="#add_venta_popup" class="add_venta_popup_class_old" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" last-price="'+results.rows.item(i).PrecioVtaMax+'" cajas-camion="'+results.rows.item(i).CajasCamion+'" >Historial</a></li>';
+              lista_contenido +='<a href="#add_venta_popup" class="add_venta_popup_class_old" data-rel="popup" codigo-venta="'+results.rows.item(i).CodArt+'" last-price="'+results.rows.item(i).PrecioVtaMax+'" cajas-camion="'+results.rows.item(i).CajasCamion+'" calibre="'+results.rows.item(i).CajasCamion+'" cant-empaque="'+results.rows.item(i).CajasCamion+'"  >Historial</a></li>';
               // lista_contenido +='<option value="'+results.rows.item(i).CodArt+'">'+results.rows.item(i).CodArt+' - '+results.rows.item(i).DesArt+'</option>'; 
 
             }
