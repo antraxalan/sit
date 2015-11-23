f25d30a2ff83f368ab749508dcd182af66624fad
antraxalan <aruquipa.alan@gmail.com>
2015-11-23 10:32:40 -0400

    carrito_pruebas2

--- js/venta/temp_table_venta.js
+++ js/venta/temp_table_venta.js
@@ -12,6 +12,7 @@ var v_save_cod_mar;
 var v_save_des_art;
 
 function insertDB_temp_db(tx) {
+  alert("ingreso a la transaccion");
   tx.executeSql('INSERT INTO TEMP_VENTA (IdArt,IdCli,Calibre,Empaque,Precio,Caja,Unidad,CajasCamion,CodMarca,DesArt) VALUES ('+v_save_art_id+','+v_cli_id+','+v_save_cal+','+v_save_emp+','+v_save_pre+','+v_save_caj+','+v_save_uni+','+v_save_caj_cam+','+v_save_cod_mar+','+v_save_des_art+')');
   var arr_art = JSON.parse(localStorage.art);
   var lon=arr_art.length;
@@ -25,7 +26,6 @@ function errorCB1_carg_tmp(err) {
 }
 
 function db_temp_guardar_venta(save_art_id,cli_id,save_cal,save_emp,save_pre,save_caj,save_uni,save_caj_cam,save_cod_mar,save_des_art) {
-
    v_save_art_id   =save_art_id;
    v_cli_id        =cli_id;
    v_save_cal      =save_cal;
@@ -36,6 +36,7 @@ function db_temp_guardar_venta(save_art_id,cli_id,save_cal,save_emp,save_pre,sav
    v_save_caj_cam  =save_caj_cam;
    v_save_cod_mar  =save_cod_mar;
    v_save_des_art  =save_des_art;
+   alert("ingreso funcion");
 
   db.transaction(insertDB_temp_db, errorCB1_carg_tmp);
 
