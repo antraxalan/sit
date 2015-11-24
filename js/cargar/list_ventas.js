e707849ba58efe7565a95b5a755561f4cd9f5455
antraxalan <aruquipa.alan@gmail.com>
2015-11-24 17:10:07 -0400

    try_lists_)

--- js/cargar/list_ventas.js
+++ js/cargar/list_ventas.js
@@ -112,7 +112,7 @@ var cantidad_ids;
 
 
             if($.inArray( results.rows.item(i).CodArt, arr_art_carrito )==-1){
-            if($.inArray( results.rows.item(i).CodArt, ids_old )==-1) ){
+            if($.inArray( results.rows.item(i).CodArt, ids_old )==-1) {
 
               var co_ma= results.rows.item(i).CodMarca;
               // if(co_ma!=15 || co_ma!=18 || co_ma!=20 || co_ma!=25 || co_ma!=30 || co_ma!=34 || co_ma!=35 || co_ma!=40 || co_ma!=50 || co_ma!=55 || co_ma!=64 || co_ma!=65 || co_ma!=66 || co_ma!=67 || co_ma!=68 || co_ma!=69 || co_ma!=70 || co_ma!=75 || co_ma!=80 || co_ma!=90 || co_ma!=94 || co_ma!=95 || co_ma!=96)
