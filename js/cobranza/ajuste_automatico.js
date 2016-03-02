324f0345cea422fb48af6688cf4e335102d58287
antraxalan <aruquipa.alan@gmail.com>
2016-03-02 12:48:11 -0400

    if's

--- js/cobranza/ajuste_automatico.js
+++ js/cobranza/ajuste_automatico.js
@@ -4,7 +4,8 @@ function list_cobranza_ajuste_automatico() {
   var cobro=localStorage.total_cobro;
   cobro=parseFloat(cobro);
   var cobro_aux=cobro;
-  // cobro=cobro*(-1);
+  var credito=0;
+
   var count=1;
 
   $(".editar_cobranza_class").each(function(index, el) {
@@ -17,45 +18,33 @@ function list_cobranza_ajuste_automatico() {
     var items = $('.editar_cobranza_class').length;
     var sum_aux;
     count=count+1;
-    // if(aux_nro !='19999999999'){
-    //   nrodctom_arr[count]  =aux_nro;
-    //   // monto_arr[count]     =aux_cob;
 
-    //   aux_fch = aux_fch.split("/");
-    //   aux_fch = aux_fch[2]+'-'+aux_fch[1]+'-'+aux_fch[0]+' 00:00:00.000';
-
-    //   fchvto_arr[count]    =aux_fch;
-    //   tipodctom_arr[count] =aux_dcm;
-    //   count                =count+1;
-    // }
     if (cobro!=0 && items>1){
-      if(cobro > 0 || aux_sal < 0){
-        if(cobro<aux_sal){
-          if(items=count){
-            if(aux_sal<0){
-              $('.list_cobranza').find('div[nrodctom="'+documenton+'"]').attr('cobrado',(cobro));
-              $('.list_cobranza').find(".html_cobrado[marca-cobranza-html='"+documenton+"']").html(cobro+' Bs.');
-              cobro=0;
-            }
-          }else{
-            // sum_aux=parseFloat(aux_sal+(cobro*(-1))).toFixed(2);
-            if()
-              $('.list_cobranza').find('div[nrodctom="'+documenton+'"]').attr('cobrado',(aux_sal));
-            $('.list_cobranza').find(".html_cobrado[marca-cobranza-html='"+documenton+"']").html(aux_sal+' Bs.');
-            cobro=cobro*(-1)+aux_sal;
-          }
+      if(cobro > 0 || aux_sal < 0 || credito <0){
+        credito = ((cobro*(-1))+credito).toFixed(2);
+        cobro=0;
+        if(count==items){
+          $('.list_cobranza').find('div[nrodctom="'+documenton+'"]').attr('cobrado',(credito*(-1)));
+          $('.list_cobranza').find(".html_cobrado[marca-cobranza-html='"+documenton+"']").html(credito*(-1)+' Bs.');
+          credito=0;
         }
+        if(count<items){
 
-        if(cobro>aux_sal){
-          if(items=count){
-            sum_aux=parseFloat(aux_sal+cobro).toFixed(2);
-            $('.list_cobranza').find('div[nrodctom="'+documenton+'"]').attr('cobrado',(sum_aux));
-            $('.list_cobranza').find(".html_cobrado[marca-cobranza-html='"+documenton+"']").html(sum_aux+' Bs.');
-            cobro=0;
-          }else{
+          if(aux_sal<0){
             $('.list_cobranza').find('div[nrodctom="'+documenton+'"]').attr('cobrado',(aux_sal));
             $('.list_cobranza').find(".html_cobrado[marca-cobranza-html='"+documenton+"']").html(aux_sal+' Bs.');
-            cobro=cobro*(-1)+aux_sal;
+            credito=credito+aux_sal;
+          }else{
+
+            if((credito*(-1))<=aux_sal){
+              $('.list_cobranza').find('div[nrodctom="'+documenton+'"]').attr('cobrado',((credito*(-1))));
+              $('.list_cobranza').find(".html_cobrado[marca-cobranza-html='"+documenton+"']").html((credito*(-1))+' Bs.');
+              credito=0;
+            }else{
+              $('.list_cobranza').find('div[nrodctom="'+documenton+'"]').attr('cobrado',(aux_sal));
+              $('.list_cobranza').find(".html_cobrado[marca-cobranza-html='"+documenton+"']").html(aux_sal+' Bs.');
+              credito=((credito*(-1))-aux_sal).toFixed(2);
+            }
           }
         }
       }
