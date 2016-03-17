34524c62b52c7db21577bca96c77e4ac108fc608
antraxalan <aruquipa.alan@gmail.com>
2016-03-17 19:44:04 -0400

    data

--- js/enviar_a_server/enviar.js
+++ js/enviar_a_server/enviar.js
@@ -21,7 +21,7 @@ function populateDB_detalle(tx) {
         //
         function queryDB_enviar_detalle(tx) {
             alert("queryDB_enviar_detalle");
-        	tx.executeSql('SELECT * FROM DETALLE', [], querySuccess_enviar_detalle, errorCB_enviar);
+            tx.executeSql('SELECT * FROM DETALLE', [], querySuccess_enviar_detalle, errorCB_enviar);
         }
         function queryDB_info_enviar_cliente(tx) {
         	tx.executeSql('SELECT * FROM CLIENTE', [], querySuccess_info_enviar_cliente, errorCB_enviar);
@@ -54,27 +54,55 @@ function populateDB_detalle(tx) {
         function querySuccess_enviar_detalle(tx, results) {
             $.mobile.loading("show");
             alert("querySuccess_enviar_detalle");
-          var direccion   =$(".direccion").val();
-          var codigo      =localStorage.g_username;
-          var password    =localStorage.g_password;
-          var info='detalle';
-          $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: "http://"+direccion+"/sitrans_server/sitrans_insert.php",
-            data: "codigo=" + codigo + "&password=" + password + "&info=" + info + "&data=" + results,
-            success: function (resp) {
-              $.mobile.loading("hide");
-              alert(resp);
-          },
-          error: function (e) {
-              $.mobile.loading("hide");
-            alert('error enviar');
-          }
-      });
-      }
 
-      function querySuccess_info_enviar_cliente(tx, results) {
+            var len = results.rows.length;
+            for (var i = 0; i < len; i++) {
+
+
+                    data=data+results.rows.item(i).TipoDcto+'|';
+                    data=data+results.rows.item(i).NroDcto+'|';
+                    data=data+results.rows.item(i).Apu+'|';
+                    data=data+results.rows.item(i).Fecha+'|';
+                    data=data+results.rows.item(i).FechaVto+'|';
+                    data=data+results.rows.item(i).TipoDctoM+'|';
+                    data=data+results.rows.item(i).NroDctoM+'|';
+                    data=data+results.rows.item(i).Precio+'|';
+                    data=data+results.rows.item(i).Tc+'|';
+                    data=data+results.rows.item(i).CodConcepto+'|';
+                    data=data+results.rows.item(i).CodCliente+'|';
+                    data=data+results.rows.item(i).Debe+'|';
+                    data=data+results.rows.item(i).Haber+'|';
+                    data=data+results.rows.item(i).CodArt+'|';
+                    data=data+results.rows.item(i).Dcajas+'|';
+                    data=data+results.rows.item(i).Hcajas+'|';
+                    data=data+results.rows.item(i).Dunidades+'|';
+                    data=data+results.rows.item(i).Hunidades+'|';
+
+
+                
+            }
+
+            var direccion   =$(".direccion").val();
+            var codigo      =localStorage.g_username;
+            var password    =localStorage.g_password;
+            var info='detalle';
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: "http://"+direccion+"/sitrans_server/sitrans_insert.php",
+                data: "codigo=" + codigo + "&password=" + password + "&info=" + info + "&data=" + data,
+                success: function (resp) {
+                  $.mobile.loading("hide");
+                  alert(resp);
+              },
+              error: function (e) {
+                  $.mobile.loading("hide");
+                  alert('error enviar');
+              }
+          });
+        }
+
+        function querySuccess_info_enviar_cliente(tx, results) {
         	// var tblText='<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
         	var tblContent='<form>';
         	tblContent+='<input id="filterTable-input" data-type="search">';
@@ -394,9 +422,9 @@ function populateDB_detalle(tx) {
 
 
          function querySuccess_info_enviar_borrar_transaccion(tx, results) {
-             alert("eliminado");
+           alert("eliminado");
 
-         }
+       }
 
 
 
@@ -414,7 +442,7 @@ function populateDB_detalle(tx) {
         	// alert("successCB_enviar_detalle");
         	// var db = window.openDatabase("strans_db", "1.0", "Sitrans DB", 500000);
             alert("successCB_enviar_detalle");
-        	db.transaction(queryDB_enviar_detalle, errorCB_enviar);
+            db.transaction(queryDB_enviar_detalle, errorCB_enviar);
         }
         function successCB_info_enviar_cliente() {
         	// alert("successCB_info_enviar_cliente");
@@ -452,7 +480,7 @@ function populateDB_detalle(tx) {
         function enviar_detalle() {
         	// alert("cargar_info_enviar_articulo");
             alert("enviar_detalle");
-        	db.transaction(successCB_enviar_detalle, errorCB_enviar);
+            db.transaction(successCB_enviar_detalle, errorCB_enviar);
         }
         function cargar_info_enviar_cliente() {
         	// alert("cargar_info_enviar_cliente");
